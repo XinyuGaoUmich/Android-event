43f1bc7d7e876d0035952e8a1d8ff759a689a9a7
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-06-03 16:08:08 -0400

    final version

--- app/src/main/java/com/laioffer/eventreporter/MyFirebaseMessagingService.java
+++ app/src/main/java/com/laioffer/eventreporter/MyFirebaseMessagingService.java
@@ -1,6 +1,13 @@
 package com.laioffer.eventreporter;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -30,6 +37,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         }
 
+        sendNotification(remoteMessage);
+
+
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
@@ -42,5 +52,45 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void handleNow() {
         Log.d(TAG, "Short lived task is done.");
     }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //Define pending intent to trigger activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        //Create Notification according to builder pattern
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentTitle(remoteMessage.getData().get("title"))
+                        .setContentText(remoteMessage.getData().get("description"))
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        String url = remoteMessage.getData().get("imgUri");
+
+        if(url  != null) {
+            notificationBuilder.setLargeIcon(Utils.getBitmapFromURL(url));
+        }
+
+
+        // Get Notification Manager
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Send notification
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
 }
 
